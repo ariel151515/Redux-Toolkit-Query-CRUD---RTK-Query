29182063fa1c0823b1e2c65029857133001dc04e
ariel151515 <ndcdigital2020@gmail.com>
2024-06-08 01:49:22 -0300

    ok

--- RTK-query/src/api/apiSlice.js
+++ RTK-query/src/api/apiSlice.js
@@ -10,16 +10,39 @@ export const apiSlice = createApi({
             query:() => ({
                 url:"/tasks",
                 method:"GET"
-            })
+            }),
+            providesTags:["Tasks"], // Le decimos que el trozo de codigo se llama tasks
+            transformErrorResponse: response => response.sort((a,b) => b.id - a.id) // ordena de mayor a menor
         }),
         createTask: builder.mutation({ // tipo mutation significa añadir datos
             query:(newTask) => ({
                 url:"/tasks",
                 method:"POST",
                 body:newTask,
-            })
+            }),
+            invalidatesTags:["Tasks"], // Cuando creo una tarea nuevaente tiene que ejecutar el tasks
+        }),
+        deleteTask: builder.mutation({
+            query:(id) => ({
+                url:`/tasks/${id}`,
+                method:"DELETE"
+            }),
+            invalidatesTags:["Tasks"], // Cuando creo una tarea nuevaente tiene que ejecutar el tasks
+        }),
+        updateTask:builder.mutation({
+            query:(updatedTask) => ({
+                url:`/tasks/${(updatedTask.id)}`,
+                method:"PUT",
+                body:updatedTask
+            }),
+            invalidatesTags:["Tasks"], // Cuando creo una tarea nuevaente tiene que ejecutar el tasks
         })
     })
 })
 
-export const { useGetTasksQuery, useCreateTaskMutation } = apiSlice;
+export const { 
+    useGetTasksQuery, 
+    useCreateTaskMutation, 
+    useDeleteTaskMutation,
+    useUpdateTaskMutation, // creamis las mutaciones
+} = apiSlice;
--- RTK-query/src/components/TaskList.jsx
+++ RTK-query/src/components/TaskList.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-import { useGetTasksQuery } from '../api/apiSlice';
+import { useGetTasksQuery, useDeleteTaskMutation, useUpdateTaskMutation } from '../api/apiSlice';
+
 
 const TaskList = () => {
 
-  const {data: tasks, isError, isLoading, error} = useGetTasksQuery()
+  const {data: tasks, isError, isLoading, error} = useGetTasksQuery();
+  const [deleteTask] = useDeleteTaskMutation();
+  const [updateTask] = useUpdateTaskMutation();
 
   if (isLoading) return <div>cargando...</div>;
   else if(isError) return <div>Error: {error.message}</div>;
@@ -15,12 +18,24 @@ const TaskList = () => {
       <ul>
         {tasks.map(task => (
             <li key={task.id}>
-              <h3>{task.title}</h3>
+              <h3>{task.name}</h3>
               <p>{task.description}</p>
-              <button>
+              <button onClick={() => {
+                deleteTask(task.id);
+              }}>
                 delete
               </button>
-              <input type='checkbox' id={task.id} />
+              <input type='checkbox' 
+                     id={task.id}
+                     checked={task.completed} 
+                     onChange={(e) => {
+                        updateTask({
+                            ...task,
+                            completed: e.target.checked, // le paso la tarea como parametro pero actualizando el valor del checked
+                        })
+                        console.log(e.target.checked);
+                     }}
+                     />
               <label htmlFor={task.id}>completed</label>
             </li>
         ))}
